2c48403841a655f481e7dceb3df2b518e67f2af0
Kyle Schick <Kyle Schick>
2016-12-23 08:30:01 +0000

    Basic test scene, movement, camera, and jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,30 +5,43 @@ public class PlayerController : MonoBehaviour {
     public float moveSpeed;
     public float mouseSensitivity;
     public float maxVerticalViewAngle;
+    public float jumpSpeed;
 
     CharacterController cc;
     float verticalRotation;
+    float yVelocity;
 
 	// Use this for initialization
 	void Start () {
         cc = GetComponent<CharacterController>();
         verticalRotation = 0;
+        yVelocity = 0;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        // Movement
-        float forwardSpeed = Input.GetAxis("Vertical") * moveSpeed;
-        float sideSpeed = Input.GetAxis("Horizontal") * moveSpeed;
-        Vector3 speed = transform.rotation * (new Vector3(sideSpeed, 0, forwardSpeed));
-        cc.SimpleMove(speed);
-
         // Camera movement
         float horizontalDeltaRotation = Input.GetAxis("Mouse X") * mouseSensitivity;
         transform.Rotate(0, horizontalDeltaRotation, 0);
         verticalRotation -= Input.GetAxis("Mouse Y") * mouseSensitivity;
         verticalRotation = Mathf.Clamp(verticalRotation, -maxVerticalViewAngle, maxVerticalViewAngle);
         Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
-       
-	}
+
+        // Movement
+        float forwardSpeed = Input.GetAxis("Vertical") * moveSpeed;
+        float sideSpeed = Input.GetAxis("Horizontal") * moveSpeed;
+        Vector3 vel = transform.rotation * (new Vector3(sideSpeed, 0, forwardSpeed));
+
+        // Jumping
+        yVelocity += Time.deltaTime * Physics.gravity.y;
+        if (cc.isGrounded && Input.GetAxis("Jump") > 0) {
+            Debug.Log("Jump");
+            yVelocity = jumpSpeed;
+        }
+
+        vel.y = yVelocity;
+        cc.Move(vel * Time.deltaTime);
+    }
 }
